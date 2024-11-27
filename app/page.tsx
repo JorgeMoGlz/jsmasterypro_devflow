f7188b754cf26910b4fce8951704e6565c1fd793
JorgeMoGlz <jorgemoglz95@gmail.com>
2024-11-27 11:00:47 -0600

    Assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h1 className="h1-bold">Hola</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Hola</h1>
+      <h1 className="h1-bold font-space-grotesk">Hola</h1>
+    </>
+  );
 };
 
 export default Home;
