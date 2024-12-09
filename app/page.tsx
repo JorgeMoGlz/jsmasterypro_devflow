861a35468b5e8457187b7be45cf0ba35496d3d4b
JorgeMoGlz <jorgemoglz95@gmail.com>
2024-12-09 16:56:18 -0600

    Light & Dark Theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 const Home = () => {
   return (
     <>
-      <h1 className="h1-bold">Hola</h1>
-      <h1 className="h1-bold font-space-grotesk">Hola</h1>
+      <h1>Hola perros</h1>
     </>
   );
 };
